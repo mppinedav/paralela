b4b59935f9d5903550f2786bc8bd86d1bc604c29
Monica Pineda <mppinedav@unal.edu.co>
2017-02-04 10:34:58 -0500

    project started

---
+++ Leibniz/main.cpp
@@ -0,0 +1,40 @@
+
+/* 
+ * File:   main.cpp
+ * Author: monica
+ *
+ * Created on 4 de febrero de 2017, 09:52 AM
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define NUM_THREADS  4
+
+ void *PrintHello(void *threadid)
+ {
+    long tid;
+    tid = (long)threadid;
+    printf("Hello World! It's me, thread #%ld!\n", tid);
+    pthread_exit(NULL);
+ }
+
+ int main (int argc, char *argv[])
+ {
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    for(t=0; t<NUM_THREADS; t++){
+       printf("In main: creating thread %ld\n", t);
+       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+       if (rc){
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
+       }
+    }
+
+    /* Last thing that main() should do */
+    pthread_exit(NULL);
+ }
+
